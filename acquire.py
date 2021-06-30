b2a1afbb1367ef58527d7f55dc1ef2e668764bd2
Tyler-Applegate <tyler.applegate@gmail.com>
2021-06-29 21:51:57 -0500

    fancy function festival

--- acquire.py
+++ acquire.py
@@ -28,7 +28,7 @@ def new_items():
     return pd.DataFrame(items_list)
 
 def new_stores():
-     '''
+    '''
     This function is specific to Zach's lol grocery dataset. It will itirate through each
     page of stores and return a pandas DataFrame of all stores.
     '''   
@@ -48,7 +48,7 @@ def new_stores():
     return pd.DataFrame(item_list)
 
 def new_sales():
-     '''
+    '''
     This function is specific to Zach's lol grocery dataset. It will itirate through each
     page of sales and return a pandas DataFrame of all sales.
     '''   
@@ -235,3 +235,158 @@ def carl_sagan():
     
     return df_sales
 
+
+################## Now we're getting fancy #############################################
+
+def new_items_page_range(alpha=1, omega=0):
+    '''
+    This function is specific to Zach's lol grocery dataset. It will itirate through each
+    page of items and return a pandas DataFrame of all items in the page range. The arguement alpha is used as 
+    a starting page, and defaults to 1. Omega is used as an ending page, and defaults to zero.
+    This allows for a return of a range of pages.
+    '''
+    items_list = []
+    base_url = 'https://python.zach.lol/api/v1'
+    response = requests.get(base_url + '/items')
+    data = response.json()
+    n = data['payload']['max_page']
+    
+    if omega == 0:
+        
+        for i in range(alpha, n+1):
+            url = 'https://python.zach.lol/api/v1/items?page='+str(i)
+            response = requests.get(url)
+            data = response.json()
+            page_items = data['payload']['items']
+            items_list += page_items
+            
+    else:
+        
+        for i in range(alpha, omega+1):
+            url = 'https://python.zach.lol/api/v1/items?page='+str(i)
+            response = requests.get(url)
+            data = response.json()
+            page_items = data['payload']['items']
+            items_list += page_items
+        
+    return pd.DataFrame(items_list)
+
+def new_stores_page_range(alpha=1, omega=0):
+    '''
+    This function is specific to Zach's lol grocery dataset. It will itirate through each
+    page of stores and return a pandas DataFrame of all stores in the page range. The arguement 
+    alpha is used as a starting page, and defaults to 1. Omega is used as an ending page, 
+    and defaults to zero. This allows for a return of a range of pages.
+    '''   
+    items_list = []
+    base_url = 'https://python.zach.lol/api/v1'
+    response = requests.get(base_url + '/stores')
+    data = response.json()
+    n = data['payload']['max_page']
+    
+    if omega == 0:
+        
+        for i in range(alpha, n+1):
+            url = 'https://python.zach.lol/api/v1/stores?page='+str(i)
+            response = requests.get(url)
+            data = response.json()
+            page_items = data['payload']['stores']
+            items_list += page_items
+            
+    else:
+        
+        for i in range(alpha, omega+1):
+            url = 'https://python.zach.lol/api/v1/stores?page='+str(i)
+            response = requests.get(url)
+            data = response.json()
+            page_items = data['payload']['stores']
+            items_list += page_items
+
+    return pd.DataFrame(item_list)
+
+def new_sales_page_range(alpha=1, omega=0):
+    '''
+    This function is specific to Zach's lol grocery dataset. It will itirate through each
+    page of sales and return a pandas DataFrame of all sales in the page range. The arguement 
+    alpha is used as a starting page, and defaults to 1. Omega is used as an ending page, 
+    and defaults to zero. This allows for a return of a range of pages.
+    '''   
+    items_list = []
+    base_url = 'https://python.zach.lol/api/v1'
+    response = requests.get(base_url + '/sales')
+    data = response.json()
+    n = data['payload']['max_page']
+    
+    if omega == 0:
+
+        for i in range(alpha,n+1):
+            url = 'https://python.zach.lol/api/v1/sales?page='+str(i)
+            response = requests.get(url)
+            data = response.json()
+            page_items = data['payload']['sales']
+            items_list += page_items
+            
+    else:
+        
+        for i in range(alpha,omega+1):
+            url = 'https://python.zach.lol/api/v1/sales?page='+str(i)
+            response = requests.get(url)
+            data = response.json()
+            page_items = data['payload']['sales']
+            items_list += page_items
+
+    return pd.DataFrame(items_list)
+
+def get_items_page_range(alpha=1, omega=0):
+    '''
+    This function operates on top of the new_items function. It first searches locally for a csv file.
+    If there is a local csv, it writes it into a pandas DataFrame. If there is no local csv, this function then
+    calls the new_items function, and writes that df to a local csv.
+    '''
+    if os.path.isfile('grocery_items'+'_'+str(alpha)+'_'+str(omega)+'.csv'):
+        # If csv file exists read in data from csv file.
+        df = pd.read_csv('grocery_items'+'_'+str(alpha)+'_'+str(omega)+'.csv', index_col=0)
+        
+    else:
+        # Read fresh data from db into a DataFrame
+        df = new_items_page_range(alpha, omega)
+        # Cache data
+        df.to_csv('grocery_items'+'_'+str(alpha)+'_'+str(omega)+'.csv')
+
+    return df
+
+def get_stores_page_range(alpha=1, omega=0):
+    '''
+    This function operates on top of the new_stores function. It first searches locally for a csv file.
+    If there is a local csv, it writes it into a pandas DataFrame. If there is no local csv, this function then
+    calls the new_stores function, and writes that df to a local csv.
+    '''
+    if os.path.isfile('grocery_stores'+'_'+str(alpha)+'_'+str(omega)+'.csv'):
+        # If csv file exists read in data from csv file.
+        df = pd.read_csv('grocery_stores'+'_'+str(alpha)+'_'+str(omega)+'.csv', index_col=0)
+        
+    else:
+        # Read fresh data from db into a DataFrame
+        df = new_stores_page_range(alpha, omega)
+        # Cache data
+        df.to_csv('grocery_stores'+'_'+str(alpha)+'_'+str(omega)+'.csv')
+
+    return df
+
+def get_sales_page_range(alpha=1, omega=0):
+    '''
+    This function operates on top of the new_sales function. It first searches locally for a csv file.
+    If there is a local csv, it writes it into a pandas DataFrame. If there is no local csv, this function then
+    calls the new_sales function, and writes that df to a local csv.
+    '''
+    if os.path.isfile('grocery_sales'+'_'+str(alpha)+'_'+str(omega)+'.csv'):
+        # If csv file exists read in data from csv file.
+        df = pd.read_csv('grocery_sales'+'_'+str(alpha)+'_'+str(omega)+'.csv', index_col=0)
+        
+    else:
+        # Read fresh data from db into a DataFrame
+        df = new_sales_page_range(alpha, omega)
+        # Cache data
+        df.to_csv('grocery_sales'+'_'+str(alpha)+'_'+str(omega)+'.csv')
+
+    return df
