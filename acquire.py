a6f646591ef116c0771c8a1f64a6f7ff8aaed0e3
Tyler-Applegate <tyler.applegate@gmail.com>
2021-07-01 16:36:11 -0500

    updated functions

--- acquire.py
+++ acquire.py
@@ -105,9 +105,16 @@ def opsd_csv():
     opsd = get_opsd()
     return opsd.to_csv('opsd_germany.csv')
 
+def all_groceries_csv():
+    '''
+    This function stores the merged stores data locally as a .csv
+    '''
+    sales_stores_items = new_groceries()
+    return sales_stores_items.to_csv('sales_stores_items.csv')
+
 ######################## Merge Data #############################################
 
-def all_groceries():
+def new_groceries():
     '''
     This functions reads in three different csv files as pandas DataFrames, and merges them
     into one df. It drops unnamed columns. Cannot use with other dataframes without changing 
@@ -119,7 +126,7 @@ def all_groceries():
     sales_df = pd.read_csv('grocery_sales.csv')
     sales_stores_df = pd.merge(sales_df, stores_df, left_on='store', right_on='store_id', how='left')
     sales_stores_items_df = pd.merge(sales_stores_df, items_df, left_on='item', right_on='item_id', how='left')
-    sales_stores_items_df = sales_stores_items_df.drop(columns=['Unnamed: 0_x', 'Unnamed: 0_y', 'Unnamed: 0'])
+    sales_stores_items_df = sales_stores_items_df.drop(columns=['Unnamed: 0_x', 'Unnamed: 0_y', 'Unnamed: 0', 'store', 'item'])
     
     return sales_stores_items_df
 
@@ -194,6 +201,24 @@ def get_opsd():
         df = new_opsd()
         # Cache data
         df.to_csv('opsd_germany.csv')
+
+    return df
+
+def get_groceries():
+    '''
+    This function operates on top of the new_groceries function. It first searches locally for a csv file.
+    If there is a local csv, it writes it into a pandas DataFrame. If there is no local csv, this function then
+    calls the new_groceries function, and writes that df to a local csv.
+    '''
+    if os.path.isfile('sales_stores_items.csv'):
+        # If csv file exists read in data from csv file.
+        df = pd.read_csv('sales_stores_items.csv', index_col=0)
+        
+    else:
+        # Read fresh data from db into a DataFrame
+        df = new_groceries()
+        # Cache data
+        df.to_csv('sales_stores_items.csv')
 
     return df
 
---
+++ prepare.py
@@ -0,0 +1,69 @@
+# This is my Data Preparation Module for Time Series
+
+import pandas as pd
+import requests
+import numpy as np
+from datetime import timedelta, datetime
+
+################## Grocery Functions #################################
+
+def to_datetime(df, col):
+    df[col] = pd.to_datetime(df[col]).sort_values()
+    return df
+
+def index_reset(df, col):
+    df = df.set_index(col).sort_index()
+    return df
+
+def day_of_week(df):
+    df['day_of_week'] = df.index.day_name()
+    return df
+
+def month(df):
+    df['month'] = df.index.month_name()
+    return df
+
+def update_sales(df):
+    df = df.rename(columns=({'sale_amount': 'sale_qty'}))
+    df['sale_qty'] = df['sale_qty'].astype(int)
+    return df
+
+def create_sales_total(df):
+    df['sales_total'] = df['sale_qty'] * df['item_price']
+    return df
+
+def prep_groceries(df, col):
+    df = to_datetime(df, col)
+    df = index_reset(df, col)
+    df = day_of_week(df)
+    df = month(df)
+    df = update_sales(df)
+    df = create_sales_total(df)
+    return df
+
+
+############ OPSD Functions ####################################
+
+def to_datetime(df, col):
+    df[col] = pd.to_datetime(df[col]).sort_values()
+    return df
+
+def index_reset(df, col):
+    df = df.set_index(col).sort_index()
+    return df
+
+def make_year(df):
+    df = df.index.year
+    return df
+
+def fill_missing(df):
+    df = df.fillna(value=0)
+
+def prep_opsd(df, col):
+    df = to_datetime(df, col)
+    df = index_reset(df, col)
+    df = month(df)
+    df = make_year(df)
+    df = fill_missing(df)
+    return df
+
